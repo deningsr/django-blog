e5cac60c93395968b8b4af1919200325d595a3cf
Denin Grcic <deningrcic2@gmail.com>
2021-11-13 12:59:01 -0800

    Added custom ModelAdmin classes

--- blogging/admin.py
+++ blogging/admin.py
@@ -2,5 +2,22 @@ from django.contrib import admin
 from blogging.models import Post, Category
 
 # Register your models here.
-admin.site.register(Post)
-admin.site.register(Category)
+
+
+class CategoryInline(admin.TabularInline):
+    model = Category
+
+
+class PostAdmin(admin.ModelAdmin):
+    pass
+    inlines = [
+        CategoryInline,
+    ]
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    exclude = ["posts"]
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
