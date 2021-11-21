d4748eeced56525c3d9538801ef30a84ebeeadd4
Denin Grcic <deningrcic2@gmail.com>
2021-11-20 16:51:32 -0800

    Updated urls for Class list views

--- blogging/urls.py
+++ blogging/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from blogging.views import stub_view
-from blogging.views import list_view
-from blogging.views import detail_view
+from blogging.views import ListView, BlogListView, BlogDetailView
+
 
 urlpatterns = [
-    path("posts/<int:post_id>/", detail_view, name="blog_detail"),
-    path("", list_view, name="blog_index"),
+    path("", BlogListView.as_view(), name="blog_index"),
+    path("posts/<int:pk>/", BlogDetailView.as_view(), name="blog_detail"),
 ]
