9dcdaa782c698b47a30eb7ed5a24f506e3bb362f
Denin Grcic <deningrcic2@gmail.com>
2021-11-20 16:51:49 -0800

    Replaced function views with class views

--- blogging/views.py
+++ blogging/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.template import loader
 from blogging.models import Post
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 # Create your views here.
 
@@ -17,18 +19,11 @@ def stub_view(request, *args, **kwargs):
     return HttpResponse(body, content_type="text/plain")
 
 
-def list_view(request):
-    published = Post.objects.exclude(published_date__exact=None)
-    posts = published.order_by("-published_date")
-    context = {"posts": posts}
-    return render(request, "list.html", context)
+class BlogListView(ListView):
+    model = Post
+    template_name = "blogging/list.html"
 
 
-def detail_view(request, post_id):
-    published = Post.objects.exclude(published_date__exact=None)
-    try:
-        post = published.get(pk=post_id)
-    except Post.DoesNotExist:
-        raise Http404
-    context = {"post": post}
-    return render(request, "blogging/detail.html", context)
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = "blogging/detail.html"
