f2ff171bc5b0414a9bf93e24c68825a5e721d45a
Denin Grcic <deningrcic2@gmail.com>
2021-11-26 15:50:23 -0800

    Updated rss feed url

--- blogging/urls.py
+++ blogging/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from blogging.views import stub_view
-from blogging.views import ListView, BlogListView, BlogDetailView
+from blogging.views import ListView, BlogListView, BlogDetailView, LatestEntriesFeed
 
 
 urlpatterns = [
     path("", BlogListView.as_view(), name="blog_index"),
     path("posts/<int:pk>/", BlogDetailView.as_view(), name="blog_detail"),
+    path("latest/feed/", LatestEntriesFeed()),
 ]
