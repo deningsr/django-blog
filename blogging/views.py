40cfcd708f84ecb0d2b9c6deb30e27dac79af882
Denin Grcic <deningrcic2@gmail.com>
2021-11-26 15:32:31 -0800

    Added rss feed

--- blogging/views.py
+++ blogging/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.contrib.syndication.views import Feed
+from django.urls import reverse
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.template import loader
 from blogging.models import Post
@@ -27,3 +29,21 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView):
     model = Post
     template_name = "blogging/detail.html"
+
+
+class LatestEntriesFeed(Feed):
+    title = "Latest Posts"
+    link = "/posts/"
+    description = "Updates when new posts are added"
+
+    def posts(self):
+        return Post.objects.order_by("-published_date")
+
+    def post_title(self, post):
+        return post.title
+
+    def item_description(self, item):
+        return item.description
+
+    def item_link(self, post):
+        return reverse("post", args=[post.pk])
